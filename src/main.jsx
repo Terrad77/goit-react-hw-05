56003b4050dcec0ab55b56b627141c0091b08e65
DAT <terlovoy77@gmail.com>
2024-03-05 21:23:29 +0200

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 // Імпорт стилів нормалізації
-import "modern-normalize";
-import './index.css'
+import 'modern-normalize';
+import './index.css';
 import App from './components/App/App';
-
+//npm install react-router-dom
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
