5117d5426bb3def241a330262e84689d9b69afd1
DAT <terlovoy77@gmail.com>
2024-03-11 16:37:57 +0200

    fix path to searched

--- src/components/MovieList/MoveList.jsx
+++ src/components/MovieList/MoveList.jsx
@@ -6,7 +6,12 @@ export default function MovieList({ movies }) {
     <ul className={css.moviesList}>
       {movies.map(movie => (
         <li className={css.moviesItem} key={movie.id}>
-          <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>
+          <NavLink
+            to={`/movies/${movie.id}`}
+            state={{ from: `${location.pathname}${location.search}` }}
+          >
+            {movie.title}
+          </NavLink>
         </li>
       ))}
     </ul>
--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,5 +1,5 @@
 import css from './MoviesPage.module.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { searchMovies } from '../../movies-api';
 //npm install react-hot-toast
 import toast, { Toaster } from 'react-hot-toast';
@@ -14,6 +14,11 @@ export default function MoviesPage() {
 
   const [params, setParams] = useSearchParams();
   const movieFilter = params.get('query') ?? '';
+
+  useEffect(() => {
+    handleSearch();
+    setQuery(movieFilter);
+  }, [query]);
 
   // ф-ція зміни параметрів запиту
   const changeMovieFilter = newFilter => {
@@ -52,6 +57,7 @@ export default function MoviesPage() {
         <Toaster />
         <input
           type="text"
+          name="find_movie"
           value={movieFilter}
           onChange={e => changeMovieFilter(e.target.value)}
         />
