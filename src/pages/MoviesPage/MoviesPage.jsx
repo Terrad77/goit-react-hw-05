56f1b23b97ad1d91b4c20b1a9229429e56ec7fe3
DAT <terlovoy77@gmail.com>
2024-03-12 17:01:21 +0200

    MoviePage in process

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,43 +1,85 @@
 import css from './MoviesPage.module.css';
 import { useEffect, useState } from 'react';
 import { searchMovies } from '../../movies-api';
-//npm install react-hot-toast
 import toast, { Toaster } from 'react-hot-toast';
 import { useSearchParams } from 'react-router-dom';
 import MovieList from '../../components/MovieList/MoveList';
 
 export default function MoviesPage() {
   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [value, setValue] = useState(searchParams.get('query') ?? '');
+  // const location = useLocation();
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [query, setQuery] = useState('');
 
-  const [params, setParams] = useSearchParams();
-  const movieFilter = params.get('query') ?? '';
-
-  useEffect(() => {
-    handleSearch();
-    setQuery(movieFilter);
-  }, [query]);
+  const query = searchParams.get('query') ?? ''; //отримання значень параметрів URLSearchParams.get(key), чекає 'key' = ім'я параметра, повертає його значення або null
 
   // ф-ція зміни параметрів запиту
-  const changeMovieFilter = newFilter => {
-    params.set('query', newFilter);
-    setParams(params);
-    setQuery(newFilter);
+  // const changeMovieFilter = e => {
+  //   e.prevent.default();
+  //   params.set('query', newFilter);
+  //   setParams(params);
+  //   setQuery(newFilter);
+  // };
+
+  //ф-ція зміни параметру запиту
+  const onFormSubmit = e => {
+    e.preventDefault();
+    if (value === '') {
+      // toast.dismiss();
+      toast.error('Please enter text to search movies!');
+      return alert("Can't find movies while input field is empty");
+    }
+    setSearchParams({ query: value });
   };
 
-  const handleSearch = async () => {
-    if (query === '') {
-      toast.dismiss();
-      toast.error('Please enter text to search movies!');
+  //ефект що виконується при монтуванні та при зміні значення параметру key=query
+  useEffect(() => {
+    //не виконувати якщо нема запиту (у query нема значення)
+    if (!query) {
       return;
     }
+    //коли запит є виклик ф-ції з наданням ій значення запиту
+    const fetchData = async () => {
+      const data = await fetchSearchMovie(query);
+    };
+    fetchData()
+      .then(data => {
+        // console.log('print data:', data.results);
+        if (!data) {
+          return alert(`Sorry don't found any by query: ${query}`);
+        }
+        setMovies(data.results);
+      })
+      .catch(error => alert(error));
+  }, [query]);
+
+  // ф-ція обробки input
+  const changeMovieFilter = e => {
+    setValue(e.target.value);
+  };
+
+  //ф-ція запиту до API
+  const fetchSearchMovie = async searchQuery => {
+    //   if (query === '') {
+    //     toast.dismiss();
+    //     toast.error('Please enter text to search movies!');
+    //     return;}
     try {
-      setMovies([]);
       setIsLoading(true);
-      const data = await searchMovies(movieFilter);
-      setMovies(data.results);
+      const info = await searchMovies(searchQuery);
+      console.log('info.results:', info.results);
+
+      // // Check if info and info.results are defined before setting movies
+      // if (info && info.results) {
+      setMovies(info.results);
+      // } else {
+      //   // when no results
+      //   setMovies([]);
+      //   console.log(`No results found for query: ${query}`);
+      // }
+      //
     } catch (error) {
       setError(true);
       console.error(error);
@@ -47,24 +89,24 @@ export default function MoviesPage() {
     }
   };
 
+  //ф-ція філтрації масиву movies за значенням запиту
   const filteredMovies = movies.filter(movie =>
-    movie.title.toLowerCase().includes(movieFilter.toLowerCase())
+    movie.title.toLowerCase().includes(query.toLowerCase())
   );
 
   return (
     <div className={css.container}>
-      <div className={css.search}>
+      <form className={css.form} onSubmit={onFormSubmit}>
         <Toaster />
         <input
           type="text"
           name="find_movie"
-          value={movieFilter}
-          onChange={e => changeMovieFilter(e.target.value)}
+          // value={movieFilter}
+          value={value}
+          onChange={changeMovieFilter}
         />
-        <button className={css.btnSearch} onClick={handleSearch}>
-          Search
-        </button>
-      </div>
+        <button className={css.btnSearch}>Search</button>
+      </form>
       {isLoading && <b>Loading search movies...</b>}
       {error && <b>HTTP error!</b>}
       <MovieList movies={filteredMovies} />
